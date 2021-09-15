eb8159a55266cb4b40db0301d9844834e77043b3
ekwanNTT <ethan.kwan@nttdata.com>
2021-09-15 10:59:59 -0700

    Remove cyress default files

---
+++ AutomatedTests/cypress/integration/file_controller.spec.js
@@ -0,0 +1,5 @@
+describe('File Controller Tests', () => {
+  it('Does not do much!', () => {
+    expect(true).to.equal(false)
+  })
+})
---
+++ AutomatedTests/cypress/integration/notification_controller.spec.js
@@ -0,0 +1,5 @@
+describe('Notification Controller Tests', () => {
+  it('Does not do much!', () => {
+    expect(true).to.equal(false)
+  })
+})
